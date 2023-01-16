7d272ab2051a7694278f0d217d84f98617bb5c67
jclio <fuguijar@qq.com>
2023-01-16 12:56:46 +0800

    fist

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/fufu.jpg" class="logo" alt="Vite logo" />
+    </a>
+  </div>
+  <HelloWorld msg="jclio" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
